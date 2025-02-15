66c504924c76dba5d086bf5eef476dd6fe42c42c
olga-sdev <olga.github@airpost.net>
2025-02-15 22:52:07 +0200

    Create gui.py

---
+++ Tkinter/gui.py
@@ -0,0 +1,22 @@
+"""
+GUI ->  Graphical User Interface;
+
+Mainframe -> powerful machines used primarily by large organizations for critical applications: can handle vast amounts of data and support thousands of users simultaneously.
+
+Terminal -> monochrome interface through which a user can interact with a computer for tasks such as:
+* Running scripts and programs.
+* Managing files and directories.
+* Configuring system settings.
+* Debugging and troubleshooting.
+
+Visual programming -> method of creating software using graphical elements and visual tools rather than writing text-based code.
+
+Key aspects of visual programming include:
+* Graphical User Interface (GUI) -> drag and drop elements, configure properties, and create workflows.
+* Flowcharts and Diagrams -> represent the logic and structure of the program.
+* Code Generation -> code based on the user's graphical input, often allowing for customization.
+
+Widgets -> small, interactive applications or tools that provide specific functionality or information.
+
+
+"""
