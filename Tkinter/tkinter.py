0d087c4be7b9e23782f3a1f5de574f61f35a9d1b
olga-sdev <olga.github@airpost.net>
2025-02-15 23:26:15 +0200

    Create tkinter.py

---
+++ Tkinter/tkinter.py
@@ -0,0 +1,12 @@
+"""
+In Python TkInter is a package named tkinter.
+
+tk elements:
+
+importing tkinter components;
+creating an ap’s window;
+adding a set of widgets;
+launching the event controller.
+"""
+
+import tkinter as tk
