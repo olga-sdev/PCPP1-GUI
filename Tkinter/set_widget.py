08fd293833aa4c90422954792f4f08874a0349c1
olga-sdev <olga.github@airpost.net>
2025-02-18 00:06:52 +0200

    Update set_widget.py

--- Tkinter/set_widget.py
+++ Tkinter/set_widget.py
@@ -21,12 +21,30 @@ class GridExample:
 
     node_1 = tk.Label(grid_window, text='9')
     node_2 = tk.Label(grid_window, text='12')
-    node_3 = tk.Label(grid_window, text='3')
+    node_3 = tk.Label(grid_window, text='2')
     node_4 = tk.Label(grid_window, text='6')
+    node_5 = tk.Label(grid_window, text='4')
 
     node_1.grid(row=1, column=0)
     node_2.grid(row=0, column=1)
-    node_3.grid(row=1, column=2)
+    node_3.grid(row=0, rowspan=2, column=2)
     node_4.grid(row=2, column=1)
+    node_5.grid(row=1, rowspan=2, column=2)
 
     grid_window.mainloop()
+
+
+class PackExample:
+    pack_window = tk.Tk()
+
+    node_1 = tk.Button(pack_window, text='Node Left')  # toward the window's left boundary
+    node_2 = tk.Button(pack_window, text='Node Right')  # toward the window's right boundary
+    node_3 = tk.Button(pack_window, text='Node Top')  # the widget is packed toward the window's top
+    node_4 = tk.Button(pack_window, text='Node Bottom')  # the widget is packed toward the window's bottom
+
+    node_1.pack(side=tk.LEFT, fill=tk.Y)  # expand it in the vertical direction
+    node_2.pack(side=tk.RIGHT, fill=tk.BOTH)  # expand it in both directions
+    node_3.pack(side=tk.TOP, fill=tk.X)  # expand it in the horizontal direction
+    node_4.pack(side=tk.BOTTOM, fill=tk.NONE)  # do not expand the widget (default behavior)
+
+    pack_window.mainloop()
