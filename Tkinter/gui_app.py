25cbaf390daa9249b0bb137a7e40355b06ce6d58
olga-sdev <olga.github@airpost.net>
2025-02-20 23:25:56 +0200

    Create gui_app.py

---
+++ Tkinter/gui_app.py
@@ -0,0 +1,13 @@
+"""
+Label –> a non-clickable widget with text info
+"""
+
+import tkinter as tk
+
+
+widget = tk.Tk()
+
+label = tk.Label(widget, text = "Widget Label")
+label.pack()
+
+window.mainloop()
