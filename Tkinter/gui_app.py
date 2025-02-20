c3b9d2a258077ab65612a1eee49a8237fa81c498
olga-sdev <olga.github@airpost.net>
2025-02-21 00:34:10 +0200

    Update gui_app.py

--- Tkinter/gui_app.py
+++ Tkinter/gui_app.py
@@ -1,5 +1,10 @@
 """
-Label –> a non-clickable widget with text info
+Label –> non-clickable component with text info.
+
+Frame -> non-clickable component for grouping and separating other components.
+
+IntVar class -> invisible object to store integer value and organize internal communication between different widgets.
+set() of cls IntVar store the value for it.
 """
 
 import tkinter as tk
@@ -7,7 +12,16 @@ import tkinter as tk
 
 widget = tk.Tk()
 
-label = tk.Label(widget, text = "Widget Label")
+label = tk.Label(widget, text="Widget Label")
 label.pack()
 
-window.mainloop()
+frame = tk.Frame(widget, height=40, width=120, bg="YellowGreen")
+frame.pack(fill=tk.BOTH)
+
+button = tk.Button(widget, text="Click")
+button.pack(fill=tk.BOTH)
+
+switch = tk.IntVar()
+switch.set(1)
+
+widget.mainloop()
