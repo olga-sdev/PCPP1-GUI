4aa06f3e8776ab9e9df3080d1d451bd6ced24c43
olga-sdev <olga.github@airpost.net>
2025-02-27 21:58:53 +0200

    Create widget_properties.py

---
+++ Tkinter/widget_properties.py
@@ -0,0 +1,9 @@
+"""
+When working with Tkinter in Python, you'll encounter a variety of widget properties (also known as options) to customize the appearance and behavior of widgets. 
+Common properties:
+Text Properties
+Geometry Management
+State and Behavior
+Layout Management
+Miscellaneous
+"""
