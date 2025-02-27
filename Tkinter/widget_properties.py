46ceddf77ea99433a1eacd7e657d8f82cde73b41
olga-sdev <olga.github@airpost.net>
2025-02-27 22:17:26 +0200

    Update widget_properties.py

--- Tkinter/widget_properties.py
+++ Tkinter/widget_properties.py
@@ -6,4 +6,30 @@ Geometry Management
 State and Behavior
 Layout Management
 Miscellaneous
+
+Possible ways of reading and setting widget properties’ values:
+*  dictionary which exists inside every widget -> widget['property'] = new_value
 """
+
+import tkinter as tk
+
+
+def switch_color(i):
+    global frame
+    color = frame["bg"]
+    if color == "green":
+        color = "blue"
+    else:
+        color = "green"
+    frame["bg"] = color
+
+
+widget = tk.Tk()
+
+widget.title("Tkinter Frame")
+widget.geometry("300x300")
+frame = tk.Frame(widget, bg="green")
+frame.pack(fill="both", expand=True)
+
+widget.bind_all("<Button-1>", switch_color)
+frame.mainloop()
