37fc238357855d8ad0dac2f36ad9939de8501f74
olga-sdev <olga.github@airpost.net>
2025-02-28 23:29:30 +0200

    Update widget_properties.py

--- Tkinter/widget_properties.py
+++ Tkinter/widget_properties.py
@@ -8,12 +8,14 @@ Layout Management
 Miscellaneous
 
 Possible ways of reading and setting widget properties’ values:
-*  dictionary which exists inside every widget -> widget['property'] = new_value
+* dictionary which exists inside every widget -> widget['property'] = new_value
+* widget methods: cget() -> read the property’s value, config() -> set a new value to the property
 """
 
 import tkinter as tk
 
 
+# Example with dict
 def switch_color(i):
     global frame
     color = frame["bg"]
@@ -32,4 +34,26 @@ frame = tk.Frame(widget, bg="green")
 frame.pack(fill="both", expand=True)
 
 widget.bind_all("<Button-1>", switch_color)
+frame.mainloop()
+
+
+# Example with cget() and config()
+def change_color(i):
+    global frame
+    color = frame.cget("bg")
+    if color == "orange":
+        color = "grey"
+    else:
+        color = "orange"
+    frame.config(bg=color)
+
+
+widget = tk.Tk()
+widget.title('Colored Frame')
+
+widget.geometry("100x600")
+frame = tk.Frame(widget, bg='orange')
+frame.pack(fill='both', expand=True)
+
+widget.bind('<Button-1>', change_color)
 frame.mainloop()
