0e1ab71fc47c12fb57aba863afeeb19030e22057
olga-sdev <olga.github@airpost.net>
2025-03-01 13:57:06 +0200

    Update widget_properties.py

--- Tkinter/Properties/widget_properties.py
+++ Tkinter/Properties/widget_properties.py
@@ -14,6 +14,8 @@ font_style ->
 "italic"
 "underline"
 "overstrike"
+
+Widgets sizes -> Tkinter/Properties/properties.md
 """
 
 import tkinter as tk
@@ -23,7 +25,12 @@ widget = tk.Tk()
 widget.title('Fonts Example')
 widget.geometry('400x400')
 
-button = tk.Button(widget, text='Button with Font', font=('Arial', '30', 'italic'))
+button = tk.Button(widget, text='Font', font=('Arial', '30', 'italic'))
 button.pack()
 
+button["borderwidth"] = 40
+button["highlightthickness"] = 20
+button["wraplength"] = 20
+
 widget.mainloop()
+
