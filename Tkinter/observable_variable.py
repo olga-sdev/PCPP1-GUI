c7e918bead10ff59780492ef91b33cfaacaec9fb
olga-sdev <olga.github@airpost.net>
2025-03-05 20:41:52 +0200

    Create observable_variable.py

---
+++ Tkinter/observable_variable.py
@@ -0,0 +1,13 @@
+"""
+In Tkinter, observable variables are used to create dynamic, data-bound interfaces. 
+These special variables can be observed for changes, and when they change, any widgets bound to them will automatically update:
+
+StringVar: For string values.
+IntVar: For integer values.
+DoubleVar: For floating-point values.
+BooleanVar: For boolean values.
+
+It is typed.
+
+To assign a value to an observable variable use set()
+"""
