ff0174ee01dcc3a0f0c4a8c2edf07f92e22aa275
olga-sdev <olga.github@airpost.net>
2025-03-09 22:27:16 +0200

    Update part_1.py

--- Lexicon_of_Widgets/part_1.py
+++ Lexicon_of_Widgets/part_1.py
@@ -1,1 +1,23 @@
+# Example with Button and Checkbutton properties
 
+import tkinter as tk
+
+
+def state_switcher():
+    if button['state'] == tk.DISABLED:
+        button.config(state=tk.NORMAL, bg='green')
+        button.flash()
+    else:
+        button.config(state=tk.DISABLED, bg='pink')
+
+
+widget = tk.Tk()
+switch = tk.IntVar()
+
+button = tk.Button(widget, text='Node')
+button.pack()
+
+checkbutton = tk.Checkbutton(widget, text='State Switcher', variable=switch, command=state_switcher)
+checkbutton.pack()
+
+widget.mainloop()
