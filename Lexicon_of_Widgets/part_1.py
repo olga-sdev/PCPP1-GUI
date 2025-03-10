8b2d62b11a3c286c99147c9f6d891baba622efca
olga-sdev <olga.github@airpost.net>
2025-03-10 22:32:44 +0200

    Update part_1.py

--- Lexicon_of_Widgets/part_1.py
+++ Lexicon_of_Widgets/part_1.py
@@ -21,3 +21,33 @@ checkbutton = tk.Checkbutton(widget, text='State Switcher', variable=switch, com
 checkbutton.pack()
 
 widget.mainloop()
+
+
+# Example with Button and Radiobuttons (same logic from previous example)
+
+def enable():
+    button.config(state=tk.NORMAL, bg='green')
+    button.flash()
+    radio_disable_var.set(radio_enable_var.get())
+
+
+def disable():
+    button.config(state=tk.DISABLED, bg='pink')
+    radio_enable_var.set(radio_disable_var.get())
+
+
+widget = tk.Tk()
+
+button = tk.Button(widget, text='Node')
+button.pack()
+
+radio_enable_var = tk.IntVar()
+radiobutton_enable = tk.Radiobutton(widget, text='Enable', variable=radio_enable_var, value=1, command=enable)
+radiobutton_enable.pack()
+
+radio_disable_var = tk.IntVar()
+radiobutton_disable = tk.Radiobutton(widget, text='Disable', variable=radio_disable_var, value=2, command=disable)
+radiobutton_disable.pack()
+radiobutton_disable.select()
+
+widget.mainloop()
