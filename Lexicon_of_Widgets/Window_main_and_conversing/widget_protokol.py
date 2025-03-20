b2b6d780e0b151b162a5b7ddbab1aab5326f4925
olga-sdev <olga.github@airpost.net>
2025-03-20 12:29:53 +0200

    Create widget_protokol.py

---
+++ Lexicon_of_Widgets/Window_main_and_conversing/widget_protokol.py
@@ -0,0 +1,17 @@
+"""
+In Tkinter, protocols are used to handle special events or interactions between app and the OS.
+
+"WM_DELETE_WINDOW" -> triggered when user attempts to close the application window (e.g., by clicking the "X" button).
+
+"""
+
+import tkinter as tk
+from tkinter import messagebox
+
+
+widget = tk.Tk()
+
+widget.protocol("WM_DELETE_WINDOW", lambda: widget.destroy() if messagebox.askyesno(
+            "Confirmation", "Confirm the widget to be closed.") else True)
+
+widget.mainloop()
