7c8b79981262b50c0bc7f2b6564a65b4627ed60c
olga-sdev <olga.github@airpost.net>
2025-03-20 11:48:29 +0200

    Update widget_dimensions.py

--- Lexicon_of_Widgets/Window_main_and_conversing/widget_dimensions.py
+++ Lexicon_of_Widgets/Window_main_and_conversing/widget_dimensions.py
@@ -5,6 +5,8 @@ maxsize -> widget's maximum size in Tkinter
 
 Set widget Size and Position:
 geometry -> widget's Size and Position
+
+resizable -> define widget to be flexible (True) or inflexible (False)
 """
 
 import tkinter as tk
@@ -21,6 +23,7 @@ def unhover(*args):
 widget = tk.Tk()
 widget.minsize(width=250, height=220)
 widget.maxsize(widget=900, height=900)
+widget.resizable(width=True, height=True)
 
 widget.bind("<Enter>", hover)
 widget.bind("<Leave>", unhover)
