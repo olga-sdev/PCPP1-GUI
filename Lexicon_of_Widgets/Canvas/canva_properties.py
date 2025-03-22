4ee42db344d10a33bf8dc684476f9d4f5917055d
olga-sdev <olga.github@airpost.net>
2025-03-22 20:46:03 +0200

    Create canva_properties.py

---
+++ Lexicon_of_Widgets/Canvas/canva_properties.py
@@ -0,0 +1,7 @@
+
+| Canva property  | Property meaning                                                           | 
+|-----------------|----------------------------------------------------------------------------|
+| borderwidth     | canvas border’s width in pixels (default: 2)                               | 
+| background (bg) | canvas border’s color (default: the same as the underlying window’s color) | 
+| height          | canvas height (in pixels)                                                  | 
+| width           | canvas width (in pixels)                                                   |	
