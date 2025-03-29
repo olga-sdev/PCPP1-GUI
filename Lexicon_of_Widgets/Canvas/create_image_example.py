23e82dc38994d6f52b88ada1e66d7ee345c741d7
olga-sdev <olga.github@airpost.net>
2025-03-29 18:16:23 +0200

    Update create_image_example.py

--- Lexicon_of_Widgets/Canvas/create_image_example.py
+++ Lexicon_of_Widgets/Canvas/create_image_example.py
@@ -15,3 +15,20 @@ canvas.create_image(200, 200, image=image)
 canvas.grid()
 
 window.mainloop()
+
+'''
+# Example with jpeg files
+import tkinter as tk
+import PIL
+
+
+widget = tk.Tk()
+canvas = tk.Canvas(widget, width=400, height=400, bg='dodgerblue4')
+jpg = PIL.Image.open('robo.jpg')
+image = PIL.ImageTk.PhotoImage(jpg)
+canvas.create_image(200, 200, image=image)
+
+canvas.grid()
+
+widget.mainloop()
+'''
